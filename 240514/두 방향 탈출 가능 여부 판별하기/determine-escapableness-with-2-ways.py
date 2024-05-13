42f3331378b8b28e5abb2e0e4395860db6ef5931
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-14 01:27:01 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 229ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240514/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,32 @@
+# BFS 가 더 좋음 A -> B 로 갈 수 있는지?
+# DFS 재귀호출 => 비용 증가
+
+N, M = map(int, input().split())
+
+A = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
+
+visited = [
+    [False for _ in range(M)]
+    for _ in range(N)
+]
+
+dxs = [0,1]
+dys = [1,0]
+
+def dfs(x, y):
+    visited[x][y] = True
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and A[nx][ny] == 1:
+            dfs(nx, ny)
+
+dfs(0, 0)
+
+if visited[-1][-1]:
+    print(1)
+else:
+    print(0)
