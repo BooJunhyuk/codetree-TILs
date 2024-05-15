209c1e7247da87e11440142d7c0098ebb9b150f0
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-15 17:10:40 +0900

    Title: 뿌요뿌요, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240515/뿌요뿌요/puyo-puyo.py
+++ 240515/뿌요뿌요/puyo-puyo.py
@@ -13,21 +13,24 @@ visited = [
 
 dxs = [0,1,0,-1]
 dys = [1,0,-1,0]
+
 cnt = 0
 bomb = 0
 bomb_cnt = 0
 bomb_arr = []
+
 def dfs(x,y):
     global bomb, bomb_cnt, cnt
     visited[x][y] = True
-    bomb = 0
+    
 
     for dx, dy in zip(dxs,dys):
         nx, ny = x + dx, y + dy
 
-        if 0<=nx < N and 0<=ny<N and not visited[nx][ny] and arr[nx][ny] == arr[x][y]:
+        if 0<= nx < N and 0 <= ny < N and not visited[nx][ny] and arr[nx][ny] == arr[x][y]:
             dfs(nx,ny)
             bomb += 1
+            print(bomb)
     #bomb_arr.append(bomb)
     if bomb >=4:
         bomb_cnt+=1
@@ -36,10 +39,10 @@ def dfs(x,y):
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            cnt = 0
+            bomb = 0
             dfs(i, j)
             bomb_arr.append(cnt)
 
 #print(bomb_arr)
 
-print(bomb_cnt,max(bomb_arr))
+#print(bomb_cnt,max(bomb_arr))
