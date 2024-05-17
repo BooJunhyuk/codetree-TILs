3b7966b3fa7ea1270e0b73e101519bdd0daa67bf
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-18 00:53:33 +0900

    Title: 부분문자열 위치 구하기, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/부분문자열 위치 구하기/find-location-of-substring.py
+++ 240518/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,18 +1,40 @@
+# a = input()
+# b = input()
+
+
+
+# a_l = len(a)
+# b_l = len(b)
+
+# s = []
+
+# def check(i,c):
+#     if c < b_l:
+#         if a[i+c] == b[c]:
+#             check(i+1,c+1)
+#     else:
+#         print(i)
+
+# if b in a:
+#     for i in range(a_l-b_l+1):
+#         check(i, 0)
+#         # if a[i] == b[0]:
+#         #     if a[i+1] == b[1]:
+#         #         if a[i+2] == b[2]:
+
+# else:
+#     print(-1)
+
+# # def check(s):
+# #     if 
+
+# # for i in a:
+# #     if i == b[0]:
+# #         s.append(i)
+
+
 a = input()
 b = input()
 
-
-
-a_l = len(a)
-b_l = len(b)
-
-s = []
-
-print(b in a)
-
-# def check(s):
-#     if 
-
-# for i in a:
-#     if i == b[0]:
-#         s.append(i)
+index = a.find(b)
+print(index)
