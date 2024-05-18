81fedca1cfc679740da5401284439cc5e6283f51
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-18 15:15:04 +0900

    Title: 그 요일은, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/그 요일은/the-day-of-the-day.py
+++ 240518/그 요일은/the-day-of-the-day.py
@@ -11,16 +11,20 @@ day = input()
 
 # 일수 계산
 day_sum = 0
-for i in range(m1,m2):
-    if i == m1:
-        day_sum += (days[m1] - d1 +1)
-    else:
-        day_sum +=days[i]
+if m1 != m2:
+    for i in range(m1,m2):
+        if i == m1:
+            day_sum += (days[m1] - d1 +1)
+        else:
+            day_sum +=days[i]
 
-day_sum += d2
+    day_sum += d2
+else:
+    day_sum = d2 - d1 +1
+
 
 cnt = 0
 for i in range(1, day_sum +1):
-    if A[(1+i)%7] == day:
+    if A[(0+i)%7] == day:
         cnt += 1
 print(cnt)
