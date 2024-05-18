ccdcbe97ce3425a797ce586b83a86f84b74803ec
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-18 18:41:58 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240518/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -8,10 +8,13 @@ max_val = 0
 
 for i in range(1, len(arr)):
 
-    if arr[i] > t and arr[i-1] > t:
-        cnt += 1
-        max_val = max(cnt, max_val)
+    if i > 1:
+        if arr[i] > t and arr[i-1] > t:
+            cnt += 1
+            max_val = max(cnt, max_val)
+        else:
+            cnt = 1
     else:
         cnt = 1
-
+        max_val = max(cnt, max_val)
 print(max_val)
