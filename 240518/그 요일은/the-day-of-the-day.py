f8b58a0b546c9fbbb849714e5cb9118032cb7990
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-18 14:58:19 +0900

    Title: 그 요일은, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/그 요일은/the-day-of-the-day.py
+++ 240518/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ A = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ]
 
 m1, d1, m2, d2 = map(int,input().split())
 
-days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day = input()
 
@@ -17,7 +17,7 @@ for i in range(m1,m2):
 day_sum += d2
 
 cnt = day_sum // 7
-cnt1 = day_sum%7 + 1
+cnt1 = day_sum%7
 
 if A[cnt1] == day:
     cnt+=1
