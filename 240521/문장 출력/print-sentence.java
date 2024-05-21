2fa3095dca2913514048b42c9da984eaee5e0947
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-05-21 17:27:20 +0900

    Title: 문장 출력, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240521/문장 출력/print-sentence.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.print("Hello World!");
+    }
+}
+
+//작은 따옴표가 아닌 큰따옴표를 사용해야 함에 주의해주세요
