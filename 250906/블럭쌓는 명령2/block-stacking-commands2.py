ce01303376e295469aaa541a7e0309c18ed5a635
BOO JUN HYUK <127727927+BooJunhyuk@users.noreply.github.com>
2025-09-06 20:05:57 +0900

    Title: 블럭쌓는 명령2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250906/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+commands = [
+    tuple(map(int, input().split())) 
+    for _ in range(k)
+    ]
+
+# Please write your code here.
+
+
+blocks = [0]*(n)
+
+for a, b in commands:
+    for i in range(a-1, b):
+        blocks[i] += 1
+
+
+max_num = max(blocks)
+print(max_num)
